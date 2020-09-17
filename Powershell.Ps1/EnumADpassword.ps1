39b4aa930e3773254323876dcb415393851b98bb
JP <contact@cybersamurai.dk>
2020-09-17 22:53:43 +0200

    new stuff

---
+++ Powershell.Ps1/EnumADpassword.ps1
@@ -0,0 +1,14 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+New-Object System.DirectoryServices.DirectoryEntry($SearchString, "admin", "password") 
+#Change admin and password
